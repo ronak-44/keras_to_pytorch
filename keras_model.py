a2a30f71f2e0eadec6eab76e4038e1fb358db93d
Ronak <ronakshah2040@gmail.com>
2021-07-26 11:49:25 +0530

    Keras Model

---
+++ keras_model.py
@@ -0,0 +1,38 @@
+def build_combined_categorical(FLAGS, NUM_FILTERS=32, FILTER_LENGTH1=8, FILTER_LENGTH2=12):
+   
+    XDinput = Input(shape=(100,), dtype='float32') 
+    XTinput = Input(shape=(100,), dtype='float32')
+
+
+    encode_smiles = Embedding(input_dim=FLAGS.charsmiset_size+1, output_dim=128, input_length=100)(XDinput) 
+    encode_smiles = Conv1D(filters=NUM_FILTERS, kernel_size=FILTER_LENGTH1,  activation='relu', padding='valid',  strides=1)(encode_smiles)
+    encode_smiles = Conv1D(filters=NUM_FILTERS*2, kernel_size=FILTER_LENGTH1,  activation='relu', padding='valid',  strides=1)(encode_smiles)
+    encode_smiles = Conv1D(filters=NUM_FILTERS*3, kernel_size=FILTER_LENGTH1,  activation='relu', padding='valid',  strides=1)(encode_smiles)
+    encode_smiles = GlobalMaxPooling1D()(encode_smiles)
+
+
+    encode_protein = Embedding(input_dim=FLAGS.charseqset_size+1, output_dim=128, input_length=100)(XTinput)
+    encode_protein = Conv1D(filters=NUM_FILTERS, kernel_size=FILTER_LENGTH2,  activation='relu', padding='valid',  strides=1)(encode_protein)
+    encode_protein = Conv1D(filters=NUM_FILTERS*2, kernel_size=FILTER_LENGTH2,  activation='relu', padding='valid',  strides=1)(encode_protein)
+    encode_protein = Conv1D(filters=NUM_FILTERS*3, kernel_size=FILTER_LENGTH2,  activation='relu', padding='valid',  strides=1)(encode_protein)
+    encode_protein = GlobalMaxPooling1D()(encode_protein)
+
+
+    encode_interaction = keras.layers.concatenate([XDinput, XTinput], axis=-1) 
+
+ 
+    FC1 = Dense(1024, activation='relu')(encode_interaction)
+    FC2 = Dropout(0.1)(FC1)
+    FC2 = Dense(1024, activation='relu')(FC2)
+    FC2 = Dropout(0.1)(FC2)
+    FC2 = Dense(512, activation='relu')(FC2)
+
+
+    predictions = Dense(1, kernel_initializer='normal')(FC2) 
+
+    interactionModel = Model(inputs=[XDinput, XTinput], outputs=[predictions])
+
+    interactionModel.compile(optimizer='adam', loss='mean_squared_error', metrics=[cindex_score])
+    print(interactionModel.summary())
+
+    return interactionModel
